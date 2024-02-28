28d056f4fd5388b734ed1d230f1190efc35f41bc
paulohernrique <pereirapaulo436@gmail.com>
2024-02-28 17:23:53 -0300

    introducao python

---
+++ 01-intro-python/variaveis-constantes.py
@@ -0,0 +1,40 @@
+# identificadores - letras, numeros, _
+# nao pode ser palavra reservada - if, for, class...
+
+# variaveis
+# identificador = valor
+# snake_case - variavel - palavra_palavra
+
+nome = "caio"
+idade = 10
+verdade = True
+
+# constantes nao existes
+# valor nao muda
+
+PI = 3.14
+
+# comentarios
+
+"""
+qqqqq
+qqqqq
+qqqqq
+"""
+
+# docstring - comentario de documentacao
+
+def somar(n1, n2):
+    """
+    funcao que soma dois numeros
+    """
+    return n1+n2
+
+
+# blocos de instrucao - for, if, def
+# por identacao - tudo que estiver abaixo da funcao e a 4 espacos, fara parte do bloco
+
+def somar(n1, n2):
+    return n1+n2
+
+
